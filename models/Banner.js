100dd41ecf2c8b2b39914d44bed28be30f1026e1
AmyBraswell <braswellamy@mac.com>
2020-09-29 01:46:44 -0500

    setting up to deploy to Vercel

--- models/Banner.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-const bannerSchema = new Schema({
-  banner: String,
-  createdAt: String,
-});
-
-module.exports = model('Banner', bannerSchema);
--- models/Photo.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-const photoSchema = new Schema({
-  picture: String,
-  createdAt: String,
-});
-
-module.exports = model('Photo', photoSchema);
-
